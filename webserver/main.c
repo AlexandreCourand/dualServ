fd4aba319c77c29e61bef042ef2fc6bb7f19adb3
Alexandre Courand <a.courand@hotmail.fr>
2016-02-03 17:30:43 +0100

    fin seance

--- webserver/main.c
+++ webserver/main.c
@@ -5,27 +5,31 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/wait.h>
 
 int main(void){
 	int socket_serveur = creer_serveur(8080);
 	initialiser_signaux();
+	//if(fork()==0){
 while(1){
-	int socket_client ;
-socket_client = accept ( socket_serveur , NULL , NULL );
-if ( socket_client == -1)
-{
-perror ( " accept " );
-/* traitement d ’ erreur */
-}
-/* On peut maintenant dialoguer avec le client */
+		int socket_client ;
+		socket_client = accept ( socket_serveur , NULL , NULL );
+		if ( socket_client == -1){
+			perror ( " accept " );
+			/* traitement d ’ erreur */
+		}
+		/* On peut maintenant dialoguer avec le client */
 
-const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n " ;
-int i = 0;
-for(i = 0;i<10;i++){
-sleep(1);
-write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+		const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n " ;
+		int i = 0;
+		for(i = 0;i<10;i++){
+			sleep(1);
+			write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+		}
+		
+		
+	//}
 }
-	}
 	return 0;
 } 
 
